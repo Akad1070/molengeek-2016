9a28cc17b31e92d8970f17eabbf976048db3d979
kadegno <akad@live.be>
2016-09-17 19:15:54 +0000

    Call to GoogleMap API to get all coord: OK

--- app.js
+++ app.js
@@ -5,58 +5,60 @@ function App(origin,dest){
     var URL_GEO_CORD ='https://maps.googleapis.com/maps/api/geocode/json?key='+API_KEY;
     var URL_ITINEAIR = 'https://maps.googleapis.com/maps/api/directions/json?key='+API_KEY;
     
-    var inputs = {
-        origin : {cord : null,adress:null}
-        ,destin : {cord : null,adress:null}
-    }
     var that = this; // Will  be used in callback methods
+    var _internals = {
+        origin : {place_id : null, cord : null,adress:null}
+        ,destin : {place_id : null, cord : null,adress:null}
+        ,map : null
+    };
+    
     
     // Define all getters and setters but ONLY PUBLIC GETTERS
     
     this.getAdressOrigin = function(){
-        return inputs.origin.adress;
+        return _internals.origin.adress;
     };
     
     
     (function setAdressOrigin(input){
-        inputs.origin.adress = input;
+        _internals.origin.adress = input;
         // Get the cord directly from Google Maps
         getGeoCordByAdress(input, function(cord,formattedAddress){
-            that.setCordOrigin(cord);
-            inputs.origin.adress = formattedAddress;
-        })
-    })(origin);
+            setCordOrigin(cord);
+            _internals.origin.adress = formattedAddress;
+        });
+    })(origin); // Self invoking function with params the origin given in new App(orign)
 
 
     this.getAdressDestination = function(){
-        return inputs.origin.destin;
+        return _internals.origin.destin;
     };
     
     (function setAdressDestination(input){
-        inputs.origin.destin = input;
+        _internals.origin.destin = input;
         // Get the cord directly from Google Maps
         getGeoCordByAdress(input, function(cord,formattedAddress){
-            that.setCordDestination(cord);
-            inputs.origin.destin = formattedAddress;
+            setCordDestination(cord);
+            _internals.origin.destin = formattedAddress;
         });
-    })(dest);
+    })(dest);  // Self invoking function with params the dest given in new App(..,dest)
     
 
     this.getCordOrigin = function(cord){
-        return inputs.origin.cord;
+        return _internals.origin.cord;
     };
     
     function setCordOrigin(cord){
-        inputs.origin.cord = cord;
+        _internals.origin.cord = cord;
     }
     
     
     this.getCordDestination = function(){
-        return inputs.origin.cord;
+        return _internals.origin.cord;
     };
     
     function setCordDestination(cord){
-        inputs.origin.cord = cord;
+        _internals.origin.cord = cord;
     };   
 
     
@@ -77,19 +79,19 @@ function App(origin,dest){
     
     	reqAjax.done(function (data,status) {
     	    debugger
-    		if(cbDone)	cbDone(data);
+    		if(cbDone)	return cbDone(data);
     	});
     	reqAjax.fail(function (xhr, textStatus, err) {
     	    debugger
-    		if(cbFail)	cbFail(xhr.responseJSON);
+    		if(cbFail)	return cbFail(xhr.responseJSON);
     	});
     };
     
     
     function getGeoCordByAdress(adr,cb){
         launchAjaxRequest(URL_GEO_CORD,{'address': adr},function(res){
-            cb(res.results[0].geometry)
-            console.debug(res.results);
+            console.debug(res.results)
+            cb(res.results[0].geometry.location,res.results[0].formatted_address);
         })
     };
     
@@ -115,10 +117,9 @@ $(function(){
     
     $btnSelect.click(function(e){
         e.preventDefault() ; // Cancel the default action of the btn
-        $origin = $('#adress-origin');
-        $destination  = $('#adress-dest');
+        $origin = $('#adress-origin'); // Get the origin textbox
+        $destination  = $('#adress-dest'); // Get the destination textbox
         
-        debugger
         if(!$origin.val()){ // If not text inputed for the origin
             alert('Suggest you to let this app get your current location');
             return false;
@@ -134,11 +135,11 @@ $(function(){
         
         // Init the map
         var map = new google.maps.Map($frameResult, {
-              origin: app.getCordOrigin(),
-              destination : app.getCordDestination(),
-              scrollwheel: true,
-              zoom: 8
-            });
+            origin: app.getCordOrigin(),
+            destination : app.getCordDestination(),
+            scrollwheel: true,
+            zoom: 8
+        });
         
         console.debug(map);
         // Depending on the btn selected, change the result frame
